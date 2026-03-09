737c07369fa910228e551f8c450a6c6ea5d8ed17
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-03-09 16:23:42 +0600

    added modal and made search field functionable

--- script/dashboard.js
+++ script/dashboard.js
@@ -10,6 +10,13 @@ const loadIssues = () => {
         })
 };
 
+const loadIssueDetail = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    const res = await fetch(url);
+    const details = await res.json();
+    displayIssueDetail(details.data);
+};
+
 
 // // {
 // // "id": 1,
@@ -24,6 +31,58 @@ const loadIssues = () => {
 // // "updatedAt": "2024-01-15T10:30:00Z"
 // // },
 
+
+const displayIssueDetail = (issue) => {
+    const detailsBox = document.getElementById("details-container");
+
+    // Status & Priority color select
+    const statusColor = issue.status === 'open' ? 'bg-[#00A96E]' : 'bg-[#A855F7]';
+    const priorityStyle = issue.priority === 'high' ? 'bg-[#FEECEC] text-[#EF4444]' :
+        issue.priority === 'medium' ? 'bg-[#FFF6D1] text-[#F59E0B]' : 'bg-[#EEEFF2] text-[#9CA3AF]';
+
+    detailsBox.innerHTML = `
+        <div class="space-y-3">
+            <h2 id="modal-title" class="text-2xl font-bold text-[#1F2937]">${issue.title}</h2>
+            <div class="flex items-center gap-3 text-sm text-[#64748B]">
+                <span id="modal-status"
+                    class="${statusColor} text-white px-3 py-1 rounded-full font-medium uppercase text-xs">
+                    ${issue.status}
+                </span>
+                <p> • Opened by <span id="modal-author" class="font-semibold text-[#1F2937]">${issue.author}</span> • 
+                    <span id="modal-date">${new Date(issue.createdAt).toLocaleDateString()}</span>
+                </p>
+            </div>
+        </div>
+
+        <div id="modal-labels" class="flex gap-2 pt-4">
+            ${issue.labels.map(label => `
+                <div class="flex gap-[4px] items-center rounded-full bg-[#EFF6FF] border border-[#BFDBFE] py-[2px] px-3">
+                    <p class="text-[10px] font-bold text-[#3B82F6] uppercase">${label}</p>
+                </div>
+            `).join('')}
+        </div>
+
+        <p id="modal-description" class="text-[#64748B] leading-relaxed py-6">
+            ${issue.description}
+        </p>
+
+        <div class="grid grid-cols-2 gap-8 bg-[#F8FAFC] rounded-lg p-4">
+            <div>
+                <p class="text-xs font-semibold text-[#64748B] uppercase mb-2">Assignee:</p>
+                <p id="modal-assignee" class="text-[#1F2937] font-medium">${issue.assignee || 'None'}</p>
+            </div>
+            <div>
+                <p class="text-xs font-semibold text-[#64748B] uppercase mb-2">Priority:</p>
+                <span id="modal-priority"
+                    class="${priorityStyle} px-4 py-1 rounded-full text-xs font-bold uppercase">
+                    ${issue.priority}
+                </span>
+            </div>
+        </div>
+    `;
+
+    document.getElementById("issue_modal").showModal();
+};
 
 const displayIssues = (issues) => {
     const issueContainer = document.getElementById("issue-container");
@@ -59,11 +118,11 @@ const displayIssues = (issues) => {
         card.className = `bg-white rounded border-t-4 ${borderColorClass} shadow-md`;
 
         card.innerHTML = `
-    <div class="p-4 space-y-3 border-b border-[#E4E4E7]">
+    <div onclick="loadIssueDetail(${issue.id})" class="p-4 space-y-3 border-b border-[#E4E4E7]">
         <div class="flex justify-between items-center">
-            <img class="h-full" src="./assets/${issue.status === 'open' ? 'Open-Status.png' : ' Closed- Status .png'}" alt="">
+            <img class="h-full" src="./assets/${issue.status === 'open' ? 'Open-Status.png' : 'Closed-Status.png'}" alt="">
             
-            <div class="${style.bg} ${style.text} rounded-full py-[6px] px-[25px] font-bold text-xs uppercase">
+            <div class="${style.bg} ${style.text} rounded-full py-[6px] px-[25px] font-medium text-xs uppercase">
                 ${issue.priority}
             </div>
         </div>
@@ -93,11 +152,11 @@ const displayIssues = (issues) => {
 
 const updateCount = (count) => {
     const countElement = document.getElementById("issue-count");
-    if (countElement) countElement.innerText = `${count} Issues`;
+    if (countElement) countElement.innerText = `${count}`;
 };
 
 // Buttons click handler
-const buttons = document.querySelectorAll(".btn-base");
+const buttons = document.querySelectorAll(".button");
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
         buttons.forEach(b => b.classList.remove("bg-[#4A00FF]", "text-white"));
@@ -112,3 +171,24 @@ buttons.forEach(btn => {
 });
 
 loadIssues();
+
+// Search function
+document.getElementById("btn-search").addEventListener("click", () => {
+    const input = document.getElementById("searchInput");
+    const searchValue = input.value.trim();
+
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const searchResult = data.data;
+            displayIssues(searchResult);
+            updateCount(searchResult.length);
+        })
+});
+
+
+document.getElementById("searchInput").addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        document.getElementById("btn-search").dispatchEvent(new Event('click'));
+    }
+});
